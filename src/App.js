6d818d9f0ce616df2fc7de831cf6d0b59f141593
Ashish Rai <ashishrai8880@gmail.com>
2022-09-28 12:29:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
+import About from "./About";
+import Home from "./Home"
+import Cart from "./Cart"
+import Contact from "./Contact"
+import Products from "./Products"
+import SingleProduct from "./SingleProduct"
+import ErrorPage from "./ErrorPage";
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return (
+    <Router>
+
+      <Routes>
+
+        <Route path="/" element={<Home/>} />
+
+        <Route path="/about" element={<About/>} />
+
+        <Route path="/cart" element={<Cart/>} />
+
+        <Route path="/products" element={<Products/>} />
+
+        <Route path="/contact" element={<Contact/>} />
+
+        <Route path="/singleproduct/:id" element={<SingleProduct/>} />
+
+        <Route path="*" element={<ErrorPage/>} />
+
+      </Routes>
+
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>OOPS !! Error has been occured</div>
+  )
+}
+
+export default ErrorPage
